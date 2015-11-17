55ceec05272bbc9ed16b2e7b1f340f9165ee5637
Cale Tilford <combinekidd@gmail.com>
2015-11-17 23:18:37 +0000

    Add Bootstrap and embed video

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return "Palpitate"
-
+    return render_template('homepage.html')
+    
 if __name__ == "__main__":
     app.run(debug = True, host='0.0.0.0', port=8080)
